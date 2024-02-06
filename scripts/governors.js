409d5025a8a6c0b8b084ef9a90a52b0a76fe99f9
Lucas Bresson <pandaboy816@gmail.com>
2024-02-06 14:38:43 -0600

    governer select added

--- scripts/governors.js
+++ scripts/governors.js
@@ -3,7 +3,8 @@ import { getGoverners } from "./database.js";
 const governors = getGoverners();
 
 export const governorsHTML = () => {
-  let htmlString = `<label for= "governor-names">Choose a governor</label>`;
+  let htmlString = `<label for= "governor-names">Choose a governor</label>
+  <select name="governor-names" id="governor-names">-- Choose Governor--</select>`;
   htmlString += `<select name="governor-names" id="governor-names">`;
 
   for (const governor of governors) {
