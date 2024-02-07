57ccf22db3dc861a8162ae4a60dd4521eec9e1be
Lucas Bresson <pandaboy816@gmail.com>
2024-02-07 11:31:47 -0600

    gitthingthingy

--- scripts/governors.js
+++ scripts/governors.js
@@ -3,14 +3,14 @@ import { getGovernors } from "./database.js";
 const governors = getGovernors();
 
 export const governorsHTML = () => {
-    let htmlString = `<article id="governor-drop-down"><label for= "governor-names">Choose a governor </label>`
-    htmlString += `<select type="governor" name="governor-names" id="governor-names">`
-    htmlString += `<option value="" selected disabled hidden>Choose a governor...</option>`
-    for (const governor of governors) {
-        if (governor.isActive) {
-            htmlString += `<option data-type="governor" "data-id="${governor.id}" data-colonyid="${governor.ColonyId}" data-isActive="${governor.isActive}"value="${governor.name}">${governor.name}</option>`
-        }
+  let htmlString = `<article id="governor-drop-down"><label for= "governor-names">Choose a governor </label>`;
+  htmlString += `<select type="governor" name="governor-names" id="governor-names">`;
+  htmlString += `<option value="" selected disabled hidden>Choose a governor...</option>`;
+  for (const governor of governors) {
+    if (governor.isActive) {
+      htmlString += `<option data-type="governor" "data-id="${governor.id}" value="${governor.ColonyId}" data-isActive="${governor.isActive}"value="${governor.name}">${governor.name}</option>`;
     }
-    htmlString += `</select></article>`
-    return htmlString
-}
+  }
+  htmlString += `</select></article>`;
+  return htmlString;
+};
