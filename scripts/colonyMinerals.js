5f7be3b2229716cdaf359b1ad556ae716cd348e5
/bin/bash -c "$(curl -fsSL https://gist.githubusercontent.com/stevebrownlee/b146bf49071c46c41eddf5778b147a71/raw/a1f8f3bebf7fab75e5bfb0d1ecabc89ab22baec8/verify-installs.sh) <christinam1215@yahoo.com>
2024-02-07 10:29:32 -0600

    added more changes

--- scripts/colonyMinerals.js
+++ scripts/colonyMinerals.js
@@ -1,16 +1,26 @@
-import { getGoverners, getColonies } from "./database.js"
+import { getPurchases, getColonies } from "./database.js"
 
 const colonies = getColonies()
+const purchases = getPurchases()
 
-const addMinerals = () => {
-const colonyMinerals = []
-let htmlString = ``
-for (const colony of colonies) {
+const governorChoiceChange  = (changeEvent) => {
+    if(changeEvent.target.type === "governor") {
+        const governorColonyId = changeEvent.data.colonyid
+        addMineralsHTML(parseInt(governorColonyId))
+    }
+}
+document.addEventListener("change", governorChoiceChange)
+
+
+const addMineralsHTML = (governorColonyId) => {
+    let htmlString = ""
+    for (const colony of colonies) {
+        if(governorColonyId === colony.id) {
+            htmlString += `<h2 id="governorColony">${colony.name} Minerals</h2>`
+        }
+        // for (const purchase of purchases) {
+        //     htmlString += ``
+        // }
+    }
     
-}
-for (const mineral of colonyMinerals) {
-    htmlString += `<ul>
-    <li></li>
-    </ul>`
-}
 }
--- scripts/governors.js
+++ scripts/governors.js
@@ -4,11 +4,11 @@ const governors = getGoverners();
 
 const governorsHTML = () => {
     let htmlString = `<article id="governor-drop-down"><label for= "governor-names">Choose a governor</label>`
-    htmlString += `<select name="governor-names" id="governor-names">`
+    htmlString += `<select type="governor" name="governor-names" id="governor-names">`
     htmlString += `<option value="" selected disabled hidden>Choose a governor...</option>`
     for (const governor of governors) {
         if (governor.isActive) {
-            htmlString += `<option data-type="governor "data-id="${governor.id}" data-colonyId="${governor.colonyId}" data-isActive="${governor.isActive}"value="${governor.name}">${governor.name}</option>`
+            htmlString += `<option data-type="governor" "data-id="${governor.id}" data-colonyid="${governor.ColonyId}" data-isActive="${governor.isActive}"value="${governor.name}">${governor.name}</option>`
         }
     }
     htmlString += `</select></article>`
